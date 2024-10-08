b37657112965691a32f55a807cef7f06c5312e54
5ard33n <fardeennaseem2006@gmail.com>
2024-10-08 07:16:31 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all javascript code as newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+console.log(3 
+     +  
+     3)  //Code readability should be high
+console.log("Fardeen")
+ 
+let name = "Harry"
+let age = 18
+let isLoggedin = false
+let state;
+
+// number => 2 to the power 52
+// bigint 
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);   // object
+console.log(typeof undefined);   // undefined
