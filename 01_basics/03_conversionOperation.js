8b032bdf5aae7b901dbaf680639f5919f9d86c51
5ard33n <fardeennaseem2006@gmail.com>
2024-10-08 13:15:42 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -66,9 +66,52 @@
 
 // // // // //
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+/****************************  Operations  **************************/
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);    //power **
+// console.log(2/2);     //divide
+// console.log(2%3);     //modulo aka remainder
+
+
+let str1 = "Fardeen"
+let str2 = " Naseem"
+str3 = str1 + str2
+console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((1 + 2) * 3 / 4);
+
+
+
+// console.log(+true);
+// console.log(+"");
+    
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);                  //prefix
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);                  //postfix
+
+
+//    Where you can study the topic //
+
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
