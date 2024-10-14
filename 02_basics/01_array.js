023f12c4f8aa6d56907fc876a0ff94a82bfd4c53
5ard33n <fardeennaseem2006@gmail.com>
2024-10-14 07:10:14 +0000

    array change 1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,10 @@
+//Array
+// shallow copy = properties who same reference point
+//deep copy =  properties who does not share the same reference point
+// array copy option creates shalllow copies
+
+
+myArr = [1, 4, 70, true, "fardeen"]
+
+
+
