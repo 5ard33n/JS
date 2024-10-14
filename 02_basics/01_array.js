1bb63df7cd3cd3caeb12b35d3b32a714f01eea1d
5ard33n <fardeennaseem2006@gmail.com>
2024-10-14 08:53:12 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -3,8 +3,46 @@
 //deep copy =  properties who does not share the same reference point
 // array copy option creates shalllow copies
 
+// const Arr1 = [1,2,3,4,5,6]
+// const Arr2 = ["RyanReynolds","RyanGosling", "JakeGyrenhall"]
+// const Arr3 = new Array(3,50,78)
+// const myArr = [1, 4, 70, true, "fardeen"]
+// console.log(myArr[4]);
 
-myArr = [1, 4, 70, true, "fardeen"]
+
+//Array methods
+
+// (myArr.push(7));
+// (myArr.push(8));
+// (myArr.pop());  //last value delete
+// myArr.unshift(9);  //adds the value in the start
+// myArr.shift();  //REMOVES THE VALUE FROM THE START
+
+
+// console.log(myArr.includes(true))
+// console.log(myArr.indexOf(true))
+// console.log(myArr.indexOf(9)) //  -1 output for the value that dont exist 
+
+// const anotherArr = myArr.join();
+
+// console.log(myArr);
+// console.log(typeof myArr);
+// console.log(anotherArr);
+// console.log(typeof anotherArr); // OUTPUT TYPEOF  anotherArr => string
 
 
 
+// Slice and Splice
+
+const myArr2 = [0, 1, 2, 3, 4, 5]
+
+console.log("A ", myArr2);
+
+const myn1 = myArr2.slice(1, 3)
+console.log(myn1);                 //slice dont manipulate the arrray (range is not included)
+
+console.log("B ", myArr2);
+const myn2 = myArr2.splice(1, 3)   //splice manipulatesthe Arr   ((range is included))
+console.log(myn2);
+
+console.log("C ", myArr2);  
---
+++ 02_basics/02_array.js
@@ -0,0 +1,44 @@
+const marvel = ["Loki", "DrDoom", "Mephisto"]
+const dc = ["Joker", "Clayface", "ScareCrow"]
+
+// marvel_villians.push(dc_villians);          //#1st merging operation
+// console.log(marvel_villians);
+// console.log(marvel_villians[3][1]);         
+   
+// const all = marvel.concat(dc);              //#2st merging operation
+// console.log(all);
+
+
+// all_heroes = [...marvel,...dc]      //spread operator                //3rd merging operation
+
+// console.log(all_heroes);      
+
+
+// arrFake = [1, 2, 3, [4, 5, 6],7, [6, 7, [4, 5]]]
+
+// arrREAl = arrFake.flat(Infinity);
+// console.log(arrREAl);
+
+
+
+
+// console.log(Array.isArray([1,2,3]));
+// console.log(Array.isArray("Fardeen"));
+// console.log(Array.isArray(true));
+// console.log(Array.isArray(1));
+// console.log(Array.isArray({name: "Fardeen"}));  //interesting
+
+
+//////////////////////////////////////////////////
+
+
+// console.log(Array.from("Fardeen"));
+// console.log(Array.from(true));
+// console.log(Array.from(1));
+// console.log(Array.from({name: "Fardeen"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
