58306a40af908a22b6ce09ce6d88f20acfd0ca14
5ard33n <fardeennaseem2006@gmail.com>
2024-10-15 15:48:46 +0000

    objects 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,4 +20,49 @@ const regularUser = {
   }
 }
 
-console.log()
+// console.log(regularUser.fullname)
+// console.log(regularUser.fullname.userfullname)
+// console.log(regularUser.fullname.userfullname.firstname)
+// console.log(regularUser.fullname.userfullname.lastname)
+
+
+const obj1 = {1: "A", 2: "B", 3: "C"}
+const obj2 = {4: "D", 5: "E", 6: "F"}
+const obj3 = {7: "G", 8: "H", 9: "I"}
+
+// const superiorObj = {obj1, obj2}
+// const superiorObj = Object.assign(obj1,obj2)
+// const superiorObj = Object.assign({}, obj1, obj2, obj3)  //.assign(source, target1, target2, target3)
+
+
+// const superiorObj = {...obj1,...obj2,...obj3}
+// console.log(superiorObj);
+
+
+const users =[
+  {
+    id: 1,
+    email: "Fardeen@gmail.com" 
+  },
+  {
+    id: 2,
+    email: "Ayaan@gmail.com" 
+  },
+  {
+    id: 2,
+    email: "BladeWolf@gmail.com" 
+  },
+]
+
+// console.log(users[1].email);
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+
+
+console.log(tinderUser.hasOwnProperty("id"));
+console.log(tinderUser.hasOwnProperty("idx"));
+
+
