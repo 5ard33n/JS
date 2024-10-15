d9575c6c2713d3a52cf25acb563fda6f67eaf383
5ard33n <fardeennaseem2006@gmail.com>
2024-10-15 20:32:20 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -3,6 +3,21 @@ const tinderUser = {}
 
 tinderUser.id = "123abc"
 tinderUser.name = "Fardeen"
+tinderUser.isLoggedIn = false
 
 
-console.log(tinderUser);
+// console.log(tinderUser);
+
+const regularUser = {
+
+
+  email: "Sleepy@gmail.com",
+  fullname: {
+    userfullname: {
+      firstname: "Fardeen",
+      lastname: "Naseem"
+    }
+  }
+}
+
+console.log()
