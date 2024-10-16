1a8b14d8f3fc8c9625f1147f77165cbb47d20719
5ard33n <fardeennaseem2006@gmail.com>
2024-10-16 13:09:27 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -54,9 +54,64 @@ const result = addTwoNumbers(3,4)
 
 
 
-function loginMessage(username){
-    return `${username} logged in\n Hello ${username}`
+// function loginMessage(username = "sam"){
+//         // if (username === undefined) //same
+//         // if (!username) {
+//         //     console.log("Please Enter A Username")
+//         //     return
+//         // } 
+//          return `${username} logged in\n Hello ${username}`
     
+// }
+
+// // console.log(loginMessage("Fardeen"));
+// console.log(loginMessage());
+
+
+
+
+                              //  this is not spread but restoperator here
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(1,4,57,89,80));
+
+
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(10,20,30,40,50));
+
+
+
+
+// const user = {
+//     username: "Fardeen",
+//     rank: 199
+// }
+
+// function objHandle(anyObject){
+
+//     console.log(`username is ${anyObject.username} and rank ${anyObject.rank}`);
+
+// }
+
+// objHandle(user)
+
+
+// objHandle({
+//     username: "Ayaan",
+//     rank: 1
+// })
+
+
+
+const Arry = [10, 20, 30, 40, 50]
+
+function returnSecondValue(Ary){
+    return Ary[1]
 }
 
-console.log(loginMessage("Fardeen"));
+// console.log(returnSecondValue(Arry));
+console.log(returnSecondValue([347, 500, 4560]));
