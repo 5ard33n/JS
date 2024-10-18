eef3cf521e4a1908831c70c76d8da00431da1731
5ard33n <fardeennaseem2006@gmail.com>
2024-10-18 05:41:03 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,18 +5,51 @@
 */
 
 
-//var c = 300                               //global scope
-let a = 300
-if (true) {
-    let a = 10       // block scope
-    const b = 20    //block scope
-    // console.log("INNER: ", a);           
+// //var c = 300                               //global scope
+// let a = 300
+// if (true) {
+//     let a = 10       // block scope
+//     const b = 20    //block scope
+//     // console.log("INNER: ", a);           
      
-}
+// }
 
 
 
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Fardeen"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);               //closure = as the two function is accesssing one function
+    }
+    // console.log(website);
+
+    //  two()
+
+}
+
+// one()
+
+
+
+// if (true) {
+//     const username = "fardeen"
+//     if (username === "fardeen") {
+//         const website = " youtube"
+//         console.log(username + website);
+//     }
+//     // console.log(website);
+//     console.log(username)
+// }
+// // console.log(username)
+
+
+
+
+//=========================== INTERESTING ================================//
 
