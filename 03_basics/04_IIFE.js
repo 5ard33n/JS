8dc8cffbc63af49056ab10b8bb0193875a1ae2ea
5ard33n <fardeennaseem2006@gmail.com>
2024-10-19 06:23:04 +0000

    iife

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -1,1 +1,14 @@
 // IMMEDIATELY INVOKED FUNCTION EXPRESSIONS (IIFE)
+
+
+//NAMED IIFE
+(function MESSAGEPNT(){
+    console.log("DATABASE CONNECTED");
+}) ();
+
+
+// IIFE in arrow function
+
+( (name) => {
+    console.log(`DATABASE TWO CONNECTED ${name}`);
+} )("FARDEEN");
